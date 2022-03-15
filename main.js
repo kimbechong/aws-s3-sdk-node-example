7c9a5790588e76e5eedae55ccc4107567be5bb55
Kim Chong <kimbechong@gmail.com>
2022-03-15 15:59:10 -0700

    Allow public access to file

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 // Import required AWS SDK clients and commands for Node.js.
-import { PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, CreateBucketCommand, PutBucketPolicyCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./client.js";
 
 // Set the parameters
 const params = {
-  Bucket: "<BUCKET_NAME>", // The name of the bucket. For example, 'sample_bucket_101'.
-  Key: "<FILENAME>", // The name of the object. For example, 'sample_upload.txt'.
-  Body: "<BODY_CONTENT>", // The content of the object. For example, 'Hello world!".
+  Bucket: "kimbechong", // The name of the bucket. For example, 'sample_bucket_101'.
+  Key: "sample.txt", // The name of the object. For example, 'sample_upload.txt'.
+  Body: "Hello world!", // The content of the object. For example, 'Hello world!".
 };
+
+const publicPolicy = {
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "public-read",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": [
+              "s3:GetObject",
+              "s3:GetObjectVersion"
+          ],
+          "Resource": "arn:aws:s3:::kimbechong/*"
+      }
+  ]
+}
 
 const run = () => {
   s3Client
@@ -17,6 +33,12 @@ const run = () => {
     })
     .then(() => {
       return s3Client.send(new PutObjectCommand(params));
+    })
+    .then(() => {
+      return s3Client.send(new PutBucketPolicyCommand({
+        Bucket: params.Bucket,
+        Policy: JSON.stringify(publicPolicy)
+      }));
     })
     .then(() => {
       console.log(
